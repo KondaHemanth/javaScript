3f89976b05c15ee5fb679f58bb22a9aab7e129f7
KondaHemanth <pavanshemanth@gmail.com>
2020-02-04 23:09:24 +0530

    The very first

---
+++ app.js
@@ -0,0 +1,17 @@
+let val;
+
+const list = document.querySelector('ul.collection');
+const listItem = document.querySelector('li.collection-item:first-child');
+
+// 1 - Element
+// 2 - Attribute
+// 3 - Text Node
+// 8 - Comment
+// 9 - Document itself
+// 10 - Doctype
+
+
+
+val = listItem;
+
+console.log(val);
